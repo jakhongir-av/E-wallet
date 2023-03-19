aa5a4f9c5719e6834dd7f71afb744fd926f4c2e9
jakhongir-av <jakhongir.av@gmail.com>
2023-03-19 21:00:21 +0500

    modify-header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Main from "./pages/main/Main";
+import Modify from "./pages/modify/Modify";
 import SignIn from "./pages/signIn/SignIn";
 import SignUp from "./pages/signUp/SignUp";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Modify />
     </div>
   );
 }
---
+++ src/pages/modify/Modify.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ModifyHeader from '../../components/ModifyHeader/ModifyHeader'
+import './Modify.css'
+
+function Modify() {
+  return (
+    <div>
+      <ModifyHeader />
+    </div>
+  )
+}
+
+export default Modify

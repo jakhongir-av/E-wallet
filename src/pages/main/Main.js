caa773814a2d859a68f47f5ea37932736df632e6
jakhongir-av <jakhongir.av@gmail.com>
2023-05-01 21:46:10 +0500

    initial commit

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -3,12 +3,14 @@ import React from "react";
 import LandingPageHeader from "../../components/LandingPageHeader/LandingPageHeader";
 import Statistics from "../../components/Statistics/Statistics";
 import Monitoring from "../../components/Monitoring/Monitoring";
+import History from '../../components/History/History'
 
 function Main() {
   return (
     <div className="back__of_main">
       <LandingPageHeader />
       <Statistics />
+      <History />
     </div>
   );
 }
